5d49338c48693ee21c1588115f75a298e9f9092d
VefaKosova <230316001@ogr.cbu.edu.tr>
2024-02-14 17:27:56 +0300

    2.4

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = "Fabulous!"
     }
-
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+    }
+    
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+    }
+    
 }
 
